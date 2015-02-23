9bbc5b5a565b52f3ee89f634aa53c2e49457e118
Jeanne Locker <jeannelocker@earthlink.net>
2015-02-23 08:39:23 -0500

    cowstomping???

---
+++ lab/makelake.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.io.*;
+
+public class makelake{
+
+    int R, C, E, N;
+    int[][] input, lake;
+
+    public makelake(){
+	Scanner sc = new Scanner(new File("makelake.dat"));
+	int j = 0;
+	while (sc.hasNext()){
+	    String line = sc.nextLine();
+	    for (int i=0;i<maxX;i++){
+	    }
+    }
+}
