2a7809c0ccb6720a1f59dbfaaf48d8800848faf1
Jeanne Locker <jeannelocker@earthlink.net>
2015-03-16 08:22:35 -0400

    hw begun

--- linkedlist/llist.java
+++ linkedlist/llist.java
@@ -1,8 +1,8 @@
-public class llist{
-    private node l;
+public class llist {
+    private node l=null;
 
     public void add(String s){
-        node tmp = new node(s);
+	node tmp = new node(s);
 	tmp.setNext(l);
 	l = tmp;
     }
@@ -10,10 +10,29 @@ public class llist{
     public String toString(){
 	String s = "";
 	node tmp;
-	for (tmp = l; tmp != null; tmp = tmp.getNext()){
+	for (tmp = l; tmp != null; tmp=tmp.getNext()){
 	    s = s + tmp + " --> ";
 	}
 	s = s + "null";
 	return s;
     }
+
+    public node find(int n){
+	int c = 0;
+	node tmp;
+	node result = tmp;
+	while (c < n){
+	    result = tmp.getNext();
+	    c++;
+	}
+	return result;
+    }
+ 
+
+    public void insert(int n, String s){
+	int c = 0;
+	node tmp;
+	tmp.find(n).add(s);
+    }
+		
 }
