10714b22be95073a243581b6774d9adc22e332a5
Jeanne Locker <jeannelocker@earthlink.net>
2015-03-23 08:40:25 -0400

    whatever

---
+++ myStack.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+
+public class myStack<E>{
+    private Node<E> l;
+
+    public myStack(){
+	l = new Node<E>();
+    }
+
+    public void push(E data){
+
+    }
+
+    public E pop(){
+
+    }
+
+    public boolean empty(){
+
+    }
+
+    public E peek(){
+
+    }
+}
