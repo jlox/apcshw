34b1b48357c61f61a33c1102e3718a6942a09050
Jeanne Locker <jeannelocker@earthlink.net>
2015-03-25 08:39:39 -0400

    queue stuff

---
+++ 11-queue/Driver.java
@@ -0,0 +1,11 @@
+public class Driver{
+    public static void main(String[] args){
+	myQueue<Integer> q = new myQueue<Integer>();
+	System.out.println(q.empty());
+	q.enqueue(5);
+	q.enqueue(6);
+	q.enqueue(7);
+	System.out.println(q.empty);
+	System.out.println(
+    }
+}
