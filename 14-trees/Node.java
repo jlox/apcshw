bcccc6dcf8bd0e3d8be6168da245ba2465f357d4
Jeanne Locker <jeannelocker@earthlink.net>
2015-04-27 08:38:01 -0400

    did this work?

--- 14-trees/Node.java
+++ 14-trees/Node.java
@@ -1,24 +1,43 @@
-public class Node {
-    private int data;
-    private Node left, right;
+public class Node{
+		private int data;
+		private Node left,right;
 
-    // constructor???
+		public void setLeft(Node n){
+				left = n;
+		}
+		public void setRight(Node n){
+				right = n;
+		}
+		public Node getLeft(){
+				return left;
+		}
+		public Node getRight(){
+				return right;
+		}
+		public int getData(){
+				return data;
+		}
 
-    public Node getLeft(){
-	return left;
-    }
+		public Node(int n){
+				data = n;
+		}
+		public Node(int n,Node l, Node r){
+				data = n;
+				left = l;
+				right = r;
+		}
+		public String toString(){
+				return ""+data;
+		}
 
-    public void setLeft(Node n){
-	left = n;
-    }
-
-    public Node getRight(){
-	return right;
-    }
-
-    public void setRight(Node n){
-	right = n;
-    }
-		
 }
+
+
+
+
+
+
+
+
+
 
