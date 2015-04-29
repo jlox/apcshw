d1b25d43d295cf6a58c095baca2e34e99bd98247
Jeanne Locker <jeannelocker@earthlink.net>
2015-04-29 08:38:33 -0400

    homework started

--- 14-trees/BST.java
+++ 14-trees/BST.java
@@ -4,7 +4,7 @@ public class BST{
     Node r;
 
     public String toString(){
-	return r + traverse(r.getRight()) + traverse(r.getLeft());
+	return traverse(r);
     }
 
     public String traverse(Node t){
@@ -12,10 +12,58 @@ public class BST{
 	if (t==null){
 	    return "";
 	} else {
-	    s = s + t.getData()
-		+ traverse(t.getLeft())
+	    s = s + traverse(t.getLeft())
+		+ t.getData() + ", " 
 		+ traverse(t.getRight());
 	    return s;
+	}
+    }
+
+    /*
+      1. get a pointer T to the node to delete and its parent T2
+      (same thing as search part of insert, but you stop at the node to delete.)
+      a) T is a leaf - point T2's left or right to null
+      b) T has one child - point T2's left or right to T's only child
+      c) T has two children
+         Find the largest in the left subtree or the smallest in the right
+	 L = L.getLeft()
+	 while (L.getRight()!=null){
+	       L = L.getRoght();
+	 }
+	 copy the data from L into T
+	 remove(T.getLefT(), L.getData())
+    */
+
+    public void remove(Node T, int data){
+	Node t2=null;
+	Node t = r;
+	if (r==null){
+	    r=n;
+	    return;
+	}
+								
+	while (t!=null){
+	    t2 = t;
+	    if (t.getData()==data)
+		return;
+	    else if (t.getData() < data)
+		t=t.getRight();
+	    else if (t.getData() > data)
+		t=t.getLeft();
+	    else
+		return;
+	}
+
+	if (t.getLeft() == null && t.getRight() == null){
+	    if (t.getData() > t2.getData()){
+		t2.setRight(null);
+	    } else {
+		t2.setLeft(null);
+	    }
+	} else if (t.getLeft() == null && t.getRight() != null){
+	    t2.setRight(t.getRight());
+	} else if (t.getRight() == null && t.getLeft() != null){
+	    t2.setLeft(t.getLeft());
 	}
     }
     
